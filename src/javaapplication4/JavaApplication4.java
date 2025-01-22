62b386f83bac91a2e6548c3d408c269ae9c02533
IHiz06 <idelacruz@solintels.com>
2025-01-21 20:27:07 -0500

    CargaN2

--- src/javaapplication4/JavaApplication4.java
+++ src/javaapplication4/JavaApplication4.java
@@ -4,13 +4,17 @@ package javaapplication4;
 import java.util.Scanner;
 public class JavaApplication4 {
     public static void main(String[] args) {
-        int n1,n2,suma;
+        int n1, n2, n3, n4, suma;
         Scanner lectura = new Scanner(System.in);
         System.out.println("Ingresar Numero 1:");
         n1 = lectura.nextInt();
-        System.out.println("Ingresar Numero 2");
+        System.out.println("Ingresar Numero 2:");
         n2 = lectura.nextInt();
-        suma  =n1 + n2;
+        System.out.println("Ingresar Numero 3:");
+        n3 = lectura.nextInt();
+        System.out.println("Ingresar Numero 4:");
+        n4 = lectura.nextInt();
+        suma = n1 + n2 + n3 +n4;
         System.out.println("Resultado:"+suma);
         
     }
